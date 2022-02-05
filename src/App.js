8cf083ea4cde51b7c256325aebf5079cd8a64e6f
OgHashBit <77114028+OgHashBit@users.noreply.github.com>
2022-02-05 16:01:15 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,27 @@
 
+import React from 'react';
+// import NavBar from "./components/NavBar";
+import './App.css';
+// import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Main from "./First/store";
+import About from "./About/about";
+import Claim from "./Contact/claim";
+
+function App() {
+  return (
+      <div className="App">
+          <div className="wrapper">
+            {/* <NavBar /> */}
+            <Main />
+            <About />
+            <Claim />
+          </div>
+      </div>
+  );
+}
+
+export default App;
+
+            // <Main />
+            // <About />
+            // <Claim />
